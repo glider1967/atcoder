c32eef1bb6838ac7ab778af214019876c250260c
glider <glider1967@protonmail.com>
2024-09-29 02:52:12 +0900

    ac-library

--- template.cpp
+++ template.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
+using mint = modint998244353;
 
 int main() {
-  int n;
-  cin >> n;
-
-  int ans = 0;
-
-  cout << ans << endl;
-  return 0;
+    int n;
+    cin >> n;
+    mint sum = 0;
+    rep(i, n) {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+    cout << sum.val() << endl;
 }
